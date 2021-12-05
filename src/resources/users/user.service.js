3a350c3368003a073d8c1c53bd018e1b74e45adb
Valentin Anikin <anival-94@mail.ru>
2021-12-05 19:01:27 +0300

    feat: add user.service.js

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,4 +2,18 @@ const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getOne = (id) => usersRepo.getOne(id);
+
+const createOne = (user) => usersRepo.createOne(user);
+
+const deleteOne = (id) => usersRepo.deleteOne(id);
+
+const updateOne = (id, newUserData) => usersRepo.updateOne(id, newUserData);
+
+module.exports = {
+    getAll,
+    getOne,
+    createOne,
+    deleteOne,
+    updateOne,
+};
