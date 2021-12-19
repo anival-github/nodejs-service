be20c967f3b4e5cbf094dc0dd048cc51315cfb54
Valentin Anikin <anival-94@mail.ru>
2021-12-19 21:24:52 +0300

    feat: move columns files from .js to .ts

---
+++ src/resources/columns/column.model.ts
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from 'uuid';
+
+interface IColumnToCreate {
+  title: string;
+  order: number;
+}
+
+class Column {
+  id: string;
+  title: string;
+  order: number;
+
+  constructor({
+    title = 'COLUMN',
+    order = 0,
+  }: IColumnToCreate) {
+    this.id = uuidv4();
+    this.title = title;
+    this.order = order;
+  }
+}
+
+export default Column;
