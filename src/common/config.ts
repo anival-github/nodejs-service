80073805cec6563ece4d8be0f961384d49392687
Valentin Anikin <anival-94@mail.ru>
2021-12-24 00:19:23 +0300

    feat: add logger level into config.ts

--- src/common/config.ts
+++ src/common/config.ts
@@ -11,6 +11,7 @@ const config = {
   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   AUTH_MODE: process.env.AUTH_MODE === 'true',
+  LOGGER_LEVEL: process.env.LOGGER_LEVEL || '4',
 };
 
 export default config;
