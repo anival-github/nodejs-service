308c8cc8282d8972b5ca6e4963ea412dbcdc4a45
Valentin Anikin <anival-94@mail.ru>
2022-01-16 23:15:36 +0300

    feat: add config variables

--- src/common/config.ts
+++ src/common/config.ts
@@ -12,6 +12,13 @@ const config = {
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   AUTH_MODE: process.env.AUTH_MODE === 'true',
   LOGGER_LEVEL: process.env.LOGGER_LEVEL || '4',
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_USER: process.env.POSTGRES_USER,
+  POSTGRES_DB: process.env.POSTGRES_DB,
+  PGDATA: process.env.PGDATA,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+  POSTGRES_PORT: Number(process.env.POSTGRES_PORT) || 5433,
+  DB_TYPE: process.env.DB_TYPE || "postgres",
 };
 
 export default config;
