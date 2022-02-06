ecde95d803edbb380da20afce6cd12c6a8ffd329
Valentin Anikin <anival-94@mail.ru>
2022-02-06 23:35:07 +0300

    feat: change eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,27 @@
-export default {
-  "parser": "@typescript-eslint/parser",
-  "root": true,
-  "env": {
-    "es2020": true,
-    "jasmine": true,
-    "jest": true,
-    "node": true
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaVersion": 2021,
-    "ecmaFeatures": {
-      "impliedStrict": true
-    },
-    "project": "tsconfig.json",
-    "tsconfigRootDir": __dirname,
-  },
-  "settings": {
-    "noInlineConfig": true,
-    "node": {
-      "tryExtensions": [".js", ".json", ".node", ".ts"]
-    },
-    "import/resolver": {
-      "node": {
-          "extensions": [".js", ".ts"],
-          "moduleDirectory": ["node_modules", "src/"]
-      }
-    }
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:node/recommended",
-    "airbnb-base",
-    "prettier"
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  "rules": {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": ["error"],
-    "import/extensions": "off",
-    "linebreak-style": "off",
-    "node/no-unsupported-features/es-syntax": "off",
-    "no-underscore-dangle": "off",
-    "import/prefer-default-export": "off",
-    "@typescript-eslint/no-explicit-any": "error",
-    "tsdoc/syntax": "warn",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"],
-    "class-methods-use-this": ["off"],
-    "node/no-missing-import": ["error", {
-      "tryExtensions": [".js", ".json", ".node", ".ts"]
-    }]
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
-  "plugins": [
-    "@typescript-eslint",
-    "eslint-plugin-tsdoc"
-  ]
-}
+};
